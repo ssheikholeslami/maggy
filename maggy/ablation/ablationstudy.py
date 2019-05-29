c9bc5efc31395af0a2d454ffd72f210401a47a21
Sina Sheikholeslami <ssheikholeslami@gmail.com>
2019-05-29 08:54:52 +0200

    feature methods added

--- maggy/ablation/ablationstudy.py
+++ maggy/ablation/ablationstudy.py
@@ -5,14 +5,13 @@ import random
 class AblationStudy(object):
     def __init__(self, **kwargs):
         self.features = Features()
-
         # TODO call the featurestore and save the list of features of the dataset
 
 
 class Features(object):
     # TODO type-checking for all the methods
     def __init__(self):
-        self.included_features = set()  # TODO set or list?
+        self._included_features = set()  # TODO set or list?
 
     def include(self, *args):
         for arg in args:
@@ -24,17 +23,30 @@ class Features(object):
 
     def _include_single_feature(self, feature):
         if type(feature) is str:
-            self.included_features.add(feature)  # TODO should check with the list retrieved from the featurestore
-            print("included {}".format(feature))  # TODO this still prints even if was duplicate
+            self._included_features.add(feature)  # TODO should check with the list retrieved from the featurestore
+            # print("included {}".format(feature))  # this still prints even if was duplicate
         else:
             raise ValueError("features.include() only accepts strings or lists of strings, "
                              "but it received {0} which is of type '{1}'."
                              .format(str(feature), type(feature).__name__))
 
-    def exclude(self, feature):
-        if feature in self.included_features:
-            self.included_features.remove(feature)
+    def exclude(self, *args):
+        for arg in args:
+            if type(arg) is list:
+                for feature in arg:
+                    self._exclude_single_feature(feature)
+            else:
+                self._exclude_single_feature(arg)
+
+    def _exclude_single_feature(self, feature):
+        if type(feature) is str:
+            if feature in self._included_features:
+                self._included_features.remove(feature)
+        else:
+            raise ValueError("features.exclude() only accepts strings or lists of strings, "
+                             "but it received {0} which is of type '{1}'."
+                             .format(str(feature), type(feature).__name__))
 
     def list_all(self):
-        for feature in self.included_features:
+        for feature in self._included_features:
             print(feature)
