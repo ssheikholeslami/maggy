7e4eeb6cac9f9abc94ba71fcae8371badd6ca9e7
Sina Sheikholeslami <ssheikholeslami@gmail.com>
2019-06-10 15:26:39 +0200

    fix leftover line in get_number_of_trials in loco

--- maggy/ablation/ablator/loco.py
+++ maggy/ablation/ablator/loco.py
@@ -15,7 +15,6 @@ class LOCO(AbstractAblator):
         self.base_dataset_generator = self.get_dataset_generator(ablated_feature=None)
 
     def get_number_of_trials(self):
-        feature_ablation_trials = len(self.ablation_study.included_features)
         return len(self.ablation_study.features.included_features) + \
             len(self.ablation_study.model.layers.included_layers)
 
