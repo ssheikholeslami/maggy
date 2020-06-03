e07a49c5f4f74eb59fb6d5d1a924629392053a5e
moritzmeister <meister.mo@gmail.com>
2020-06-03 14:55:36 +0200

    fix call to base model generator with hparams

--- maggy/ablation/ablator/loco.py
+++ maggy/ablation/ablator/loco.py
@@ -132,7 +132,7 @@ class LOCO(AbstractAblator):
         def model_generator(*args):
             import tensorflow as tf
 
-            base_model = base_model_generator()
+            base_model = base_model_generator(**self.hparams)
 
             list_of_layers = [
                 base_layer for base_layer in base_model.get_config()["layers"]
